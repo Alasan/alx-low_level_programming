42e20ded42885016ff506711efe7e7d9a7afb636
Alasan <liebility@gmail.com>
2022-10-15 14:11:34 -0700

    3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main -Entry point
  *
  * Return: Always 0 (success)
  */
 
-
 int main(void)
+
 {
 	char c;
 
-	c = 'a';	
+	char d;
+	
+	c = 'a';
+	d = 'A';
 	while
 		(c <= 'z') {
+
 			putchar(c);
 			c++;
 		}
+	while
+		(d <= 'z'){
+			putchar(d);
+			d++;
+		}
 	putchar('\n');
 	return (0);
-}
+
+}	
